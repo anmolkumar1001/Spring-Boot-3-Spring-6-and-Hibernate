42bfb687bd512111e2c3ac6f18731d2d81c918d5
Anmol Kumar <1RN20IS020@RNSIT.onmicrosoft.com>
2025-08-10 22:44:29 +0530

    added PATCH method

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/anmol/springboot/cruddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/anmol/springboot/cruddemo/rest/EmployeeRestController.java
@@ -106,5 +106,20 @@ public class EmployeeRestController {
         return objectMapper.convertValue(employeeNode, Employee.class);
     }
 
+    // add mapping for DELETE /employee/{employeeId} - delete employee
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId) {
+
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        // throw exception if null
+        if(tempEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        employeeService.deleteById(employeeId);
+
+        return "Deleted employee id - " + employeeId;
+    }
 
 }
