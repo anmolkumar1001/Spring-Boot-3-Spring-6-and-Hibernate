3d385376672d9c055cf9f0c914952b3093481a69
Anmol Kumar <1RN20IS020@RNSIT.onmicrosoft.com>
2025-08-10 21:42:18 +0530

    rest crud emp

---
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/anmol/springboot/cruddemo/rest/EmployeeRestController.java
@@ -0,0 +1,66 @@
+package com.anmol.springboot.cruddemo.rest;
+
+import com.anmol.springboot.cruddemo.dao.EmployeeDAO;
+import com.anmol.springboot.cruddemo.entity.Employee;
+import com.anmol.springboot.cruddemo.service.EmployeeService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+    private EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeRestController(EmployeeService theEmployeeService, ObjectMapper theObjectMapper) {
+        employeeService = theEmployeeService;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> findAll() {
+        return employeeService.findAll();
+    }
+
+    // add mapping for GET /employeeId - get employee with id
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if(theEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        return theEmployee;
+    }
+
+    // add mapping for POST /employees - add new employee
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee) {
+
+        // also just in case they pass an id in JSON ... set id to 0
+        // this is to force a save of new item ... instead of update
+
+        theEmployee.setId(0);
+
+        Employee dbEmployee = employeeService.save(theEmployee);
+
+        return dbEmployee;
+    }
+
+    // add mapping for PUT /employees - update existing employee
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee) {
+
+        Employee dbEmployee = employeeService.save(theEmployee);
+
+        return dbEmployee;
+    }
+
+
+}
---
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/test/java/com/anmol/springboot/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anmol.springboot.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
