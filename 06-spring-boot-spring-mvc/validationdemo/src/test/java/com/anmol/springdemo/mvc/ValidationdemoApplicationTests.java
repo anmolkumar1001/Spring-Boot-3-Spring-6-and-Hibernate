ee551dd2d194fa867f12a6bda5e1ed77375d4c3a
Anmol Kumar <1RN20IS020@RNSIT.onmicrosoft.com>
2025-08-24 23:25:55 +0530

    spring-mvc-validation

---
+++ 06-spring-boot-spring-mvc/validationdemo/src/test/java/com/anmol/springdemo/mvc/ValidationdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anmol.springdemo.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
